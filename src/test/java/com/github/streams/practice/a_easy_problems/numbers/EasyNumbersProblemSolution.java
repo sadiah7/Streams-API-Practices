4f255c433bb5ac752017bb37eae977da429ea039
Zahid Khan <xahiidkhan@gmail.com>
2026-05-14 16:09:24 +0530

    refactor(numbers): remove unused helper methods from easy solution

--- src/test/java/com/github/streams/practice/a_easy_problems/numbers/EasyNumbersProblemSolution.java
+++ src/test/java/com/github/streams/practice/a_easy_problems/numbers/EasyNumbersProblemSolution.java
@@ -1,11 +1,8 @@
 package com.github.streams.practice.a_easy_problems.numbers;
 
 import java.util.*;
-import java.util.function.IntPredicate;
-import java.util.function.IntUnaryOperator;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 public class EasyNumbersProblemSolution {
 
@@ -37,47 +34,11 @@ public class EasyNumbersProblemSolution {
     return input.stream().distinct().toList();
   }
 
-  public static long sumOfNumbers(List<Integer> input) {
-    return input.stream().reduce(0, Integer::sum);
-  }
-
-  public static long sumOfUniqueNumbers(List<Integer> input) {
-    return input.stream().distinct().reduce(0, Integer::sum);
-  }
-
-  public static List<Integer> segregateEvenOddNumbers(Stream<Integer> input) {
-    return input.collect(Collectors.partitioningBy(x -> x % 2 == 0)).values().stream()
-        .flatMap(Collection::stream)
-        .toList();
-  }
-
-  public static long sumOfDigits(int input) {
-    final IntPredicate isDivisibleBy10 = number -> number > 0;
-    final IntUnaryOperator divideBy10 = number -> number / 10;
-
-    System.out.println("Input number : " + input);
-
-    return IntStream.iterate(input, isDivisibleBy10, divideBy10).map(x -> x % 10).sum();
-  }
-
   public static int reverseANumber(int input) {
     System.out.println("Input number : " + input);
     return IntStream.iterate(input, x -> x > 0, x -> x / 10)
         .map(x -> x % 10)
         .reduce(0, (value, previous) -> value * 10 + previous);
-  }
-
-  public static List<Integer> generateIntStreamOfPrimeNumberStream(final int limit) {
-    final IntPredicate isPrime =
-        number ->
-            IntStream.rangeClosed(2, (int) Math.sqrt(number))
-                .noneMatch(divisor -> number % divisor == 0);
-
-    return IntStream.iterate(2, x -> x < Integer.MAX_VALUE, x -> x + 1)
-        .filter(isPrime)
-        .limit(limit)
-        .boxed()
-        .toList();
   }
 
   public static List<List<Integer>> convertInt2DArrayToList(int[][] input) {
